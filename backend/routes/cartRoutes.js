69f07ef231314de9ece0a943ad9a0b746abee80f
Muhammed Nemathullah Khan <115334110+Nemathullahkhan@users.noreply.github.com>
2025-04-30 10:29:54 +0530

    Removed redundant code

--- backend/routes/cartRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { addToCart } from "../controllers/cartController.js";
-
-const router = Router();
-
-router.post("/add", addToCart)  
-
-export default router;
